6fd0e47a2a25ac2380621cdf8dfb33f69d64e1d4
chheunseangmeng <seangmeng.chheun@student.passerellesnumeriques.org>
2025-06-14 15:36:19 +0700

    added authors into AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,63 +2,103 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Cache\Repository;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+    // list down author fakke data-------------------
+    public $authors = [
+        ['id' => 'author_001', 'name' => 'SeaVMengzz', 'bio' => 'I am an author of Khmer story', 'nationality' => 'Khmer'],
+        ['id' => 'author_002', 'name' => 'YoYozz', 'bio' => 'I am an author of English story', 'nationality' => 'UK'],
+        ['id' => 'author_003', 'name' => 'Ejojozz', 'bio' => 'I am an author of France story', 'nationality' => 'french'],
+    ];
+
+    // To show all authors and the empty for handle error -------------------
     public function index()
     {
-        //
+        if (!empty($this->authors)) {
+            return response()->json([
+                'message' => 'Here are all authors',
+                'data' => $this->authors
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No authors found'
+            ], 404);
+        }
+    }
+    // show author by id
+    public function showAuthor(string $id)
+    {
+        foreach ($this->authors as $author) {
+            if ($author['id'] == $id) {
+                return response()->json([
+                    'message' => 'Here is the author',
+                    'data' => $author
+                ], 200);
+            }
+        }
+
+        // if cannot find author----
+        return response()->json([
+            'message' => 'Author not found'
+        ], 404);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // create author 
+    public function createAuthor(Request $request)
     {
-        //
+        $newAuthor = [
+            'id' => $request->input('id'),
+            'name' => $request->input('name'),
+            'bio' => $request->input('bio'),
+            'nationality' => $request->input('nationality'),
+        ];
+
+        return response()->json([
+            'message' => 'Author created successfully',
+            'data' => $newAuthor
+        ], 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    // edit existing author
+    public function editAuthor(Request $request, string $id)
     {
-        //
+        foreach ($this->authors as $key => $author) {
+            if ($author['id'] === $id) {
+                $this->authors[$key]['name'] = $request->input('name');
+                $this->authors[$key]['bio'] = $request->input('bio');
+                $this->authors[$key]['nationality'] = $request->input('nationality');
+
+                return response()->json([
+                    'message' => 'Author updated successfully',
+                    'data' => $this->authors[$key]
+                ], 200);
+            }
+        }
+
+        return response()->json([
+            'message' => 'Author not found'
+        ], 404);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    // Delete an author by id
+    public function deleteAuthor(string $id)
     {
-        //
-    }
+        foreach ($this->authors as $key => $author) {
+            if ($author['id'] === $id) {
+                unset($this->authors[$key]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+                return response()->json([
+                    'message' => 'Author deleted successfully'
+                ], 200);
+            }
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'message' => 'Author not found'
+        ], 404);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// I use prefix and group api------------------\
+// I use prefix and group api ( library )------------------\
 Route::prefix("/library")->group(function() {
     Route::get("/books", [BookController::class, "index"])->name("allBooks");
     Route::get("/books/{id}", [BookController::class, "showBook"]);
     Route::post("/books", [BookController::class, "createBook"]);
     Route::put("/books/{id}", [BookController::class, "editBook"]);
     Route::delete("/books/{id}", [BookController::class, "deleteBook"]);
+});
+// ---------------------------------------------------
+
+// I use prefix and group api ( author )------------------\
+Route::prefix("/author")->group(function() {
+    Route::get("/authors", [AuthorController::class, "index"])->name("allAuthor");
+    Route::get("/authors/{id}", [AuthorController::class, "showAuthor"]);
+    Route::post("/authors", [AuthorController::class, "createAuthor"]);
+    Route::put("/authors/{id}", [AuthorController::class, "editAuthor"]);
+    Route::delete("/authors/{id}", [AuthorController::class, "deleteAuthor"]);
 });
 // ---------------------------------------------------
 
