4369d90159d75c6c80d31b39b42b8c24f96cf922
chheunseangmeng <seangmeng.chheun@student.passerellesnumeriques.org>
2025-06-14 14:22:45 +0700

    added data into BookController

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// I use prefix and group api------------------\
+Route::prefix("/library")->group(function() {
+    Route::get("/books", [BookController::class, "index"])->name("allBooks");
+    Route::get("/books/{id}", [BookController::class, "showBook"]);
+    Route::post("/books", [BookController::class, "createBook"]);
+    Route::put("/books/{id}", [BookController::class, "editBook"]);
+    Route::delete("/books/{id}", [BookController::class, "deleteBook"]);
+});
+// ---------------------------------------------------
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
